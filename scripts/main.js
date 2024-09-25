75cc015bc33ab852c371da2ab98b1114906e70cf
Son Tran <taigamefreeaz@gmail.com>
2024-09-25 15:37:52 +0700

    Thêm sản phẩm

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ const products = [
   { id: 1, name: "Đồng hồ siêu đẹp", price: 100000, image: "images/Watch1.jpg", description: "Mô tả sản phẩm 1" },
   { id: 2, name: "Apple Watch vô giá", price: 200000, image: "images/Watch2.jpg", description: "Mô tả sản phẩm 2" },
   { id: 3, name: "Đồng hồ rẻ tiền", price: 300000, image: "images/Watch3.jpg", description: "Mô tả sản phẩm 3" },
+  { id: 4, name: "Lightstick J97", price: 997000, image: "images/lightstickj97.jpg", description: "???" },
 ];
 
 // Hiển thị danh sách sản phẩm
